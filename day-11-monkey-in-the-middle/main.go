62d117e45f7e39bc472fcc83aa1010266147796c
Levi Ceccato <levi.ceccato@gmail.com>
2023-01-27 10:43:30 +1100

    Add part 2

--- day-11-monkey-in-the-middle/main.go
+++ day-11-monkey-in-the-middle/main.go
@@ -13,6 +13,48 @@ func main() {
 
 	// Parse input
 
+	monkeys1 := parseMonkeys(input)
+	monkeys2 := parseMonkeys(input)
+
+	// Run rounds
+
+	roundCount := 20
+	business := startMonkeyBusiness(3, roundCount, monkeys1, 0)
+
+	// Output result
+
+	fmt.Printf("Monkey business level for %d rounds: %d\n", roundCount, business)
+
+	// Rerun rounds
+
+	divisibleBysCommonMultiple := 1
+	for _, monkey := range monkeys2 {
+		divisibleBysCommonMultiple *= monkey.test.divisibleBy
+	}
+
+	roundCount = 10_000
+	business = startMonkeyBusiness(0, roundCount, monkeys2, divisibleBysCommonMultiple)
+
+	// Output result
+	fmt.Printf("Monkey business level for %d rounds: %d\n", roundCount, business)
+}
+
+type monkey struct {
+	inspectionCount int
+	itemWorryLevels queue[int]
+	operation       monkeyOperation
+	test            monkeyTest
+}
+
+type monkeyOperation struct {
+	operand, operator string
+}
+
+type monkeyTest struct {
+	divisibleBy, trueMonkey, falseMonkey int
+}
+
+func parseMonkeys(input []byte) []*monkey {
 	var monkeys []*monkey
 
 	for _, paragraph := range bytes.Split(input, []byte("\n\n")) {
@@ -69,16 +111,14 @@ func main() {
 		monkeys = append(monkeys, &newMonkey)
 	}
 
-	// Set data
+	return monkeys
+}
 
-	roundCount := 20
-	relief := 3
-
-	// Run rounds
-
+func startMonkeyBusiness(relief, roundCount int, monkeys []*monkey, commonMultiple int) int {
 	for round := 1; round <= roundCount; round++ {
 		for _, monkey := range monkeys {
-			for _, worryLevel := range monkey.itemWorryLevels.values() {
+			worryLevels := monkey.itemWorryLevels.values()
+			for _, worryLevel := range worryLevels {
 				monkey.itemWorryLevels.dequeue()
 				monkey.inspectionCount++
 
@@ -103,7 +143,11 @@ func main() {
 					newLevel = worryLevel * operand
 				}
 
-				newLevel = newLevel / relief
+				if commonMultiple == 0 {
+					newLevel /= relief
+				} else {
+					newLevel %= commonMultiple
+				}
 
 				// Calculate target monkey
 
@@ -130,24 +174,7 @@ func main() {
 		monkeyBusiness *= monkey.inspectionCount
 	}
 
-	// Output result
-
-	fmt.Printf("Monkey business level: %d\n", monkeyBusiness)
-}
-
-type monkey struct {
-	inspectionCount int
-	itemWorryLevels queue[int]
-	operation       monkeyOperation
-	test            monkeyTest
-}
-
-type monkeyOperation struct {
-	operand, operator string
-}
-
-type monkeyTest struct {
-	divisibleBy, trueMonkey, falseMonkey int
+	return monkeyBusiness
 }
 
 type queue[T comparable] struct {
